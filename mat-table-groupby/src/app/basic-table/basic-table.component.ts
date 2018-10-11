b0cb3eddb0573dccdc52ead3d136870b0999b267
Stephen Turner <git@webturner.co.uk>
2018-10-11 08:55:15 +0100

    ng g c basic-table

---
+++ mat-table-groupby/src/app/basic-table/basic-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-table',
+  templateUrl: './basic-table.component.html',
+  styleUrls: ['./basic-table.component.css']
+})
+export class BasicTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
